de26cfb00bc6ac687281cb5df3ac35f496123d63
motemen <motemen@gmail.com>
2020-05-21 22:44:03 +0900

    +CharsetTransport

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/motemen/go-nuts
+
+go 1.14
+
+require (
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
+	golang.org/x/text v0.3.2
+)
---
+++ httputil/charsettransport.go
@@ -0,0 +1,42 @@
+package httputil
+
+import (
+	"io"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+)
+
+// CharsetTransport is an http.Transport which automatically decodes resp.Body by its charset.
+type CharsetTransport struct {
+	Base http.RoundTripper
+}
+
+type readCloser struct {
+	io.Reader
+	io.Closer
+}
+
+// RoundTrip implements http.RoundTripper.
+func (t *CharsetTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	base := t.Base
+	if base == nil {
+		base = http.DefaultTransport
+	}
+
+	resp, err := base.RoundTrip(req)
+	if err != nil {
+		return resp, err
+	}
+
+	r, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
+	if err != nil {
+		return resp, err
+	}
+
+	resp.Body = &readCloser{
+		Reader: r,
+		Closer: resp.Body,
+	}
+	return resp, nil
+}
