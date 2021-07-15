385c459229796bfd407fb632f7de2e7e39c9fc04
motemen <motemen@gmail.com>
2021-07-15 23:04:45 +0900

    experimental: NormalizeURL

---
+++ urlutil/normalize.go
@@ -0,0 +1,107 @@
+package urlutil
+
+import (
+	"net/url"
+	pathPkg "path"
+	"strings"
+
+	"golang.org/x/net/idna"
+)
+
+var defaultPorts = map[string]string{
+	"https": "443",
+	"http":  "80",
+}
+
+// NormalizeURL normalizes URL u in such manner:
+// - all components should be represented in ASCII
+// - precent encoding in upper case
+func NormalizeURL(u *url.URL) (*url.URL, error) {
+	u.Scheme = strings.ToLower(u.Scheme)
+
+	port := u.Port()
+
+	hostname, err := idna.ToASCII(u.Hostname())
+	if err != nil {
+		return nil, err
+	}
+
+	u.Host = strings.ToLower(hostname)
+
+	if port == defaultPorts[u.Scheme] {
+		port = ""
+	}
+	if port != "" {
+		u.Host += ":" + port
+	}
+
+	path := u.RawPath
+	if path == "" {
+		path = u.Path
+	}
+
+	if path == "" {
+		path = "/"
+	}
+
+	path, err = normalizeComponent(path, "/", url.PathEscape)
+	if err != nil {
+		return nil, err
+	}
+	u.RawPath = pathPkg.Clean(path)
+	u.Path, err = url.PathUnescape(u.RawPath)
+	if err != nil {
+		return nil, err
+	}
+
+	u.RawQuery, err = normalizeComponent(u.RawQuery, "&;=", url.QueryEscape)
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func normalizeComponent(component string, special string, escape func(string) string) (string, error) {
+	escaped := ""
+
+	for component != "" {
+		var part, sep string
+		if i := strings.IndexAny(component, special); i >= 0 {
+			part, sep, component = component[:i], component[i:i+1], component[i+1:]
+		} else {
+			part, sep, component = component, "", ""
+		}
+
+		unescaped, err := url.PathUnescape(part)
+		if err != nil {
+			return "", err
+		}
+
+		escaped += escape(unescaped) + sep
+	}
+
+	return escaped, nil
+}
+
+func escapeQuery(query string) (string, error) {
+	escaped := ""
+
+	for query != "" {
+		var part, sep string
+		if i := strings.IndexAny(query, "&;="); i >= 0 {
+			part, sep, query = query[:i], query[i:i+1], query[i+1:]
+		} else {
+			part, sep, query = query, "", ""
+		}
+
+		un, err := url.QueryUnescape(part)
+		if err != nil {
+			return "", err
+		}
+
+		escaped += url.QueryEscape(un) + sep
+	}
+
+	return escaped, nil
+}
---
+++ urlutil/normalize_test.go
@@ -0,0 +1,89 @@
+package urlutil
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestNormalizeURL(t *testing.T) {
+	tests := []struct {
+		in      string
+		want    string
+		wantErr bool
+	}{
+		{
+			in:   "http://www.example.com:80/",
+			want: "http://www.example.com/",
+		},
+		{
+			in:   "https://www.example.com:443/",
+			want: "https://www.example.com/",
+		},
+		{
+			in:   "https://www.example.com:9999/",
+			want: "https://www.example.com:9999/",
+		},
+		{
+			in:   "https://はじめよう.みんな/",
+			want: "https://xn--p8j9a0d9c9a.xn--q9jyb4c/",
+		},
+		{
+			in:   "https://localhost/abc/🤗",
+			want: "https://localhost/abc/%F0%9F%A4%97",
+		},
+		{
+			in:   "https://localhost/?q=🤗",
+			want: "https://localhost/?q=%F0%9F%A4%97",
+		},
+		{
+			in:   "https://localhost/?q=%f0%9f%a4%97",
+			want: "https://localhost/?q=%F0%9F%A4%97",
+		},
+		{
+			in:   "https://localhost",
+			want: "https://localhost/",
+		},
+		{
+			in:   "HTTPS://WWW.EXAMPLE.COM/",
+			want: "https://www.example.com/",
+		},
+		{
+			in:   "https://localhost/%61/foo",
+			want: "https://localhost/a/foo",
+		},
+		{
+			in:   "https://localhost/%aa/foo",
+			want: "https://localhost/%AA/foo",
+		},
+		{
+			in:   "https://localhost/%aa/%2F",
+			want: "https://localhost/%AA/%2F",
+		},
+		{
+			in:   "https://localhost/?q=%3D&a=b",
+			want: "https://localhost/?q=%3D&a=b",
+		},
+		{
+			in:   "https://localhost/?q=foo+bar+baz",
+			want: "https://localhost/?q=foo%2Bbar%2Bbaz",
+		},
+		{
+			in:   "https://localhost/a/./b/../c",
+			want: "https://localhost/a/c",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			u, _ := url.Parse(tt.in)
+			got, err := NormalizeURL(u)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NormalizeURL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.String() != tt.want {
+				t.Errorf("NormalizeURL() = %v, want %v", got, tt.want)
+			}
+			t.Logf("%q -> %q", tt.in, got)
+		})
+	}
+}
